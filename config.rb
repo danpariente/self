b2f16fecefe9e95f6fc1e58c43927cb41336d944
Dan <dansification@gmail.com>
2020-07-25 10:34:04 -0400

    Configure live reload

--- config.rb
+++ config.rb
@@ -57,9 +57,9 @@ end
 
 page "/feed.xml", layout: false
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Helpers
 # Methods defined in the helpers block are available in templates
